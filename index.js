b188bd2f9627c52fcbaf27ccfe7a4d1cb6226cfb
Yutao-Zhou <13520759678@163.com>
2023-04-15 23:44:58 -0400

    Beta

---
+++ index.js
@@ -0,0 +1,86 @@
+// const customers = "中央广播电视总台、中央电视台CCTV7、中央人民广播电台、北京电视台、北京歌华有线广电网络、福建省广播影视集团、福建省（原）有线电视台、福建广播电视传输发射中心、广东省广播电视台、深圳市广播电视台、香港（ONE TV）亚太卫视、广西自治区广播视台、海南省广播电视总台、江西省广电网络、江西省教育电视台、湖南省广播电视台、湖南省教育电视台、湖南省微波总站、湖北教育电视台、贵州省广播电视台、贵州省广播网络、云南省广播电视台、西藏自治区广播电视台、江苏省（幸福教育频道）广播电台视、河南省广播电视台、山东省广播电视台、山东省广播电台、山东教育电视台、山西省电视台、甘肃省广电网络、甘肃省微波总站、新疆维吾尔自治区广播电视台、内蒙古自治区广播电视台、内蒙古自治区广电网络、河北省广播电视台、辽宁省传输发射中心、辽宁省彩塔中心、天津广播影视集团、天津交通广播电台、天津广电网络、吉林省电视台、陕西省（购物频道）电视台、安徽省（在线频道）电视台、宁夏回族自治区广电网络、北京市丰台区、大兴区、通州区、房山区、石景山区、朝阳区广播电视中心、北京市顺义区、密云区广播电视局、北京市海淀区信息中心、天津市滨河新区、塘沽区电视台、石家庄市电视台、邯郸市电视台、唐山市广播电视台、秦皇岛市广播电视台、承德市广电网络、张家口市广电局、下花园区广电局、衡水市广播电视台、邢台市电视台、保定市广电网络、唐山市丰润区广播电视局、河北省滦平广电局   河北省丰宁广电局、河北省迁西广电局、河北省迁安广电局、河北深泽县广播电视局、磁县有线电视台、营口市电视台、双辽市电视台、东港市广播电视台、鞍山市广播电台、大连市广播电视台、大连广播局大和尚山微波站、辽阳市广播电视台、海城市广播电视台、盘锦电视台、铁岭电视台、昌图电视台、建平县广播电视局、东营电视台、吉林市电视台、通化市广电网络、黑河电视台、大庆电视台、双鸭山广电网络、哈尔滨电视台、佳木斯电视台、鸡西电视台、延吉电视台、呼和浩特电视台、包头电视台、乌海电视台、锡林郭勒电视台、鄂尔多斯电视台、鄂温克旗电视台、赤峰电视台、通辽电视台、呼伦贝尔电视台、乎伦贝尔广电网络、巴彦淖尔电视台、内蒙赤峰平庄矿务局电视台、兰州市下属9个区县广电网络、酒泉卫星发射基地电视台、敦煌广电网络、民乐电视台、张掖电视台、武威电视台、天水电视台、庆阳电视台、武都广电网络、西峰庆阳广电网络、临夏广电网络、合作广电网络、酒泉市广电网络、石嘴山市广播电视台、宁夏传输发射中心及下属7个台站、海西州广播电视台、忠卫广播电视台、吴忠广播电视台、湟中县广播电视台、乌鲁木齐市电视台、农五师电视台、喀什电视台、克拉马依油田电视台、霍尔果斯电视台、阿勒泰电视台、巴州电视台、伊犁电视台、农十师电视台、昌吉电视台、哈密电视台、新疆西新工程43个站点、西宁电视台、拉萨市广播电视台、昌都（五县）电视台、巴塘、稻城、后渠、康定、镶塘、松潘、香城广播电视台、阿坝藏族羌族自治州电视台、青川电视台、理县电视台、汶川电视台、南充广播电视台、若尔盖电视台、叙永广播电视台、广汉电视台、什坊电视台、自贡电视台、德阳电视台、绵阳电视台、广元电视台、遂宁电视台、雅安电视台、乐山电视台、甘孜藏族自治州电视台、武隆广播电视台、江油电视台、万县电视台、西安教育电视台、宝鸡广播电视台、渭南广播电视台、榆阳广播电视台、榆林广播电视台、咸阳广播电视台、安康广播电视台、铜川广播电视台、神木电视台、杨陵电视台、高陵广播电视局贵、阳市广播电视台、遵义广播电视台、毕节广播电视台、茅台集团电视台、安顺电视台、黔西南电视台、百里杜鹃电视台、独山电视台、大理广播电视台、曲靖广播电视台、昭通电视台、南宁电视台、梧州广播电视台、玉林广播电视台、北海电视台、钦州广播电视台、防城港电视台、贵港电视台、贺州广播电视台、河池广播电台、海口市广播电视台、三亚市电视台、万宁电视台、广州（花都区）电视台、韶关广播电视台、汕尾广播电视台、梅州广播电视台、阳江电视台深圳市、龙岗区）广播电视台、揭阳电视台、惠州电视台、清远广播电视台、东莞（松山湖）电视台、福州市电视台、泉州电视台、福清电视台、三明电视台、传输发射中心下属47个台站、漳州电视台、平潭电视台、厦门诏安广播电视台、安溪电视台、南昌广电网络、九江电视台、九江广电网络、宜春广电网络、抚州市临川广电网络、乐平广电网络、大余广播电视局、兴国广电网络、赣州广电网络、赣州电视台、乐安广电网络、南丰广电网络、东乡广电网络、抚州市广播电台、永新广电网络、鄱阳电视台、新余电视台、长沙广播电视局、岳阳广电网络、益阳电视台、邵阳电视台、宁乡电视台、浏阳广电网络、传输发射中心（西新工程）下属37个台站、常德电视台、湘潭广播电视台、潇湘电影制片厂电视台、永州电视台、涟钢电视台、郴州广播电视台、怀化广电网络、麻阳广播电视局、娄底广电中心、十堰电视台、潜江广播电视台、荆门广播电视台、鄂州广播电视台、随州电视台、团风电视台、中原油田电视台、宜昌电视台、黄冈广播电视台、咸宁广播电视台、恩施广播电视台、孝感电视台、温州电视台、富阳电视台、嵊州广播电视局、镇海电视台、仓南电视台、温岭电视台、龙游广播电视台、常山电视台、萧山广播电视台、慈溪广播电视台、衢州电视台、瑞安广播电视台、象山广播电视台、台州电视台、龙泉广播电视台、湖州电视台、丽水广播电视台、磐安电视台、宁波电视台、芜湖市电视台、铜陵电视台、定远广播电视台、六安电视台、安庆广播电视台、蚌埠电视台、淮南广播电视台、马鞍山电视台、滁州广播电视台、阜阳电视台、宿州广播电视台、南京广播电视台、徐州电视台、常熟电视台、连云港广播电视台、南通电视台、张家港广播电视台、盐城电视台、扬州广播电视台、镇江电视台、太仓广播电视台、上海东方明珠发射塔、上海松江区电视台、华东师大融媒体中心、郑州电视台、开封广播电视台、洛阳电视台、平顶山电视台、永煤集团电视台、许昌电视台、新郑电视台、安阳电视台、新乡广播电视台、周口电视台、鹤壁广播电视台、焦作电视台、濮阳广播电视台、漯河电视台、三门峡广播电视台、济南市广播电视台、烟台电视台、威海广播电视台、东营电视台、滨州广播电视台、乳山电视台、荷泽广播电视台、德州电视台、泰安广播电视台、济宁电视台、潍坊广播电视台、日照电视台、临沂广播电视台、龙口电视台、寿光广播电视台、莱阳电视台、太原电视台、大同广播电视台、阳泉电视台、运城广电网络、运城电视台、晋城广播电视台、朔州电视台、晋中广播电视台、忻州电视台、临汾广播电视台、吕梁电视台、长治电视台、代县广播电视局、灵丘广电局、天镇广电局、左云广播电视局、成都索贝科技股份有限公司、北京星光影视集团有限公司、北京北广电子科技集团有限公、北京四达时代集团有限公司、北京新奥特集团有限公司、北京中科（大洋）科技股份有限公司、北京格非视频技术有限公、北京正奇联讯科技有限公司、安达斯（北京）有限公司、北京冠华系统集成有限公司、北京英夫美迪科技股份有限公司、安徽励图信息科技股份有限公司、北京宇田冠泰科技有限公司、北京永兴众智科技有限公司、北京时代奥视科技有限公司"
+// Define an array of location names to geocode
+var locationNames = customers.split("、");
+var apiKey = 'API_KEY';
+let broadcastingStations = []; 
+
+fetch('customers.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // You can now access the JSON data and save it as a variable or perform further processing
+    broadcastingStations = data;
+    // Use jsonData variable for further processing or saving as needed
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
+
+var map = L.map('map').setView([39.905131, 116.461176], 12);
+
+// Add a tile layer to the map for displaying the map tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Loop through the broadcasting stations and add markers to the map
+for (var i = 0; i < broadcastingStations.length; i++) {
+  var station = broadcastingStations[i];
+  if (station !== undefined) {
+    var marker = L.marker([station.lat, station.lon]).addTo(map)
+    .bindPopup('<b>' + station.name + '</b><br>Lat: ' + station.lat + '<br>Lon: ' + station.lon);
+  }
+}
+
+
+// Create an array of promises for each fetch request
+// var promises = locationNames.map(function(locationName, index) {
+//   return fetch(`https://maps.googleapis.com/maps/api/geocode/json?language=zh&address=${locationName}&key=${apiKey}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       if (data.results.length > 0) {
+//         // Extract the latitude and longitude coordinates from the API response
+//         var latitude = data.results[0].geometry.location.lat;
+//         var longitude = data.results[0].geometry.location.lng;
+
+//         // Use the obtained coordinates in your application as needed
+//         broadcastingStations[index] = {
+//           name: locationNames[index],
+//           lat: latitude,
+//           lon: longitude
+//         };
+//       } else {
+//         console.error('Failed to geocode location:', locationName);
+//       }
+//     })
+//     .catch(error => console.error('Error geocoding location:', error));
+// });
+
+// // Wait for all promises to resolve
+// Promise.all(promises)
+//   .then(() => {
+//     console.log(broadcastingStations);
+
+//     // Create a Leaflet map centered at a specific location
+//     var map = L.map('map').setView([39.905131, 116.461176], 12);
+
+//     // Add a tile layer to the map for displaying the map tiles
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution: '© OpenStreetMap contributors'
+//     }).addTo(map);
+
+//     // Loop through the broadcasting stations and add markers to the map
+//     for (var i = 0; i < broadcastingStations.length; i++) {
+//       var station = broadcastingStations[i];
+//       if (station !== undefined) {
+//         var marker = L.marker([station.lat, station.lon]).addTo(map)
+//         .bindPopup('<b>' + station.name + '</b><br>Lat: ' + station.lat + '<br>Lon: ' + station.lon);
+//       }
+//     }
+//     function download(content, fileName, contentType) {
+//       var a = document.createElement("a");
+//       var file = new Blob([content], {type: contentType});
+//       a.href = URL.createObjectURL(file);
+//       a.download = fileName;
+//       a.click();
+//     }
+//     download(JSON.stringify(broadcastingStations), 'customers.json', 'text/plain');
+//   });
